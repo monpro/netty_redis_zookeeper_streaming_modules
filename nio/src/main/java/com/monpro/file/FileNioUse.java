2b474c0d3f357d53804b5a3536cf22c1d0a2a2fc
monpro <monproshen@gmail.com>
2020-12-28 15:43:42 +1100

    add java file nio use cases

---
+++ nio/src/main/java/com/monpro/file/FileNioUse.java
@@ -0,0 +1,81 @@
+package com.monpro.file;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileNioUse {
+
+    private static final int CAPACITY = 2048;
+
+    // read: channel - buffer
+    // writer: buffer - channel
+    public static void nioReadFile(String fileName) {
+        try {
+            RandomAccessFile file = new RandomAccessFile(fileName, "rw");
+            FileChannel fileChannel = file.getChannel();
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(CAPACITY);
+            int length = fileChannel.read(byteBuffer);
+            while (length != -1) {
+                byteBuffer.flip();
+                byte[] bytes = byteBuffer.array();
+                String string = new String(bytes, 0, length);
+                length = fileChannel.read(byteBuffer);
+                System.out.println(string);
+            }
+
+            fileChannel.close();
+            byteBuffer.clear();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void nioCopyFile(String srcPath, String destPath) {
+        File srcFile = new File(srcPath);
+        File destFile = new File(destPath);
+        
+        try {
+            if (!destFile.exists()) {
+                destFile.createNewFile();
+            }
+
+            FileInputStream fileInputStream = null;
+            FileOutputStream fileOutputStream = null;
+            FileChannel inChannel = null;
+            FileChannel outChannel = null;
+            
+            try {
+                fileInputStream = new FileInputStream(srcFile);
+                fileOutputStream = new FileOutputStream(destFile);
+                inChannel = fileInputStream.getChannel();
+                outChannel = fileOutputStream.getChannel();
+                ByteBuffer byteBuffer = ByteBuffer.allocate(2048);
+                
+                int length = inChannel.read(byteBuffer);
+                
+                while (length != -1) {
+                    byteBuffer.flip();
+                    int outLength = 0;
+                    while ((outLength = outChannel.write(byteBuffer)) != 0) {
+                        System.out.println("write bytes: " + outLength);
+                    }
+                    length = inChannel.read(byteBuffer);
+                    byteBuffer.clear();
+                }
+                outChannel.force(true);
+            }
+            finally {
+                fileInputStream.close();
+                fileOutputStream.close();
+                inChannel.close();
+                outChannel.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
