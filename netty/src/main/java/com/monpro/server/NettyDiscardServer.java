2321f70a3ba7a63a5e90d4c296a4580ca37aaa02
monpro <monproshen@gmail.com>
2020-12-29 21:12:15 +1100

    add discard server using netty framework

---
+++ netty/src/main/java/com/monpro/server/NettyDiscardServer.java
@@ -0,0 +1,54 @@
+package com.monpro.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyDiscardServer {
+    private final int serverPort;
+    ServerBootstrap bootstrap = new ServerBootstrap();
+
+    public NettyDiscardServer(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public void runServer() {
+        EventLoopGroup leaderLoopGroup = new NioEventLoopGroup();
+        EventLoopGroup workerLoopGroup = new NioEventLoopGroup();
+        // group reactors
+        bootstrap.group(leaderLoopGroup, workerLoopGroup);
+
+        bootstrap.channel(NioServerSocketChannel.class);
+        bootstrap.localAddress(serverPort);
+
+        bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+        bootstrap.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+        bootstrap.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                // it should add a handler
+                socketChannel.pipeline().addLast(null);
+            }
+        });
+
+        try {
+            // start to bind server
+            ChannelFuture channelFuture = bootstrap.bind().sync();
+            ChannelFuture closeFuture = channelFuture.channel().closeFuture();
+            closeFuture.sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally{
+            workerLoopGroup.shutdownGracefully();
+            leaderLoopGroup.shutdownGracefully();
+        }
+    }
+}
